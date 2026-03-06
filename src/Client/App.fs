e396b1b4dcc5f7ed94d74adaf125a552fcf7ef1d
Petr Pokorný <petrpokorny@microsoft.com>
2026-03-06 22:42:28 +0100

    tm-actions-9pj Extract action button functions from App.fs into ActionButtons.fs
    Created ActionButtons.fs with isCodingToolBusy, commentIcon, wrenchIcon,
    createPrIcon, actionButton, noFocusProps. Updated App.fs to open ActionButtons,
    replaced inline busy check in syncButton with isCodingToolBusy.

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -8,6 +8,7 @@ open Feliz
 open Fable.Remoting.Client
 open Browser
 open Fable.Core.JsInterop
+open ActionButtons
 
 type Model =
     { Repos: RepoModel list
@@ -443,12 +444,6 @@ let private providerDisplayName (provider: CodingToolProvider option) =
     | Some Copilot -> "Copilot"
     | None -> "Coding tool"
 
-let noFocusProps = [
-    prop.tabIndex -1
-    prop.onMouseDown (fun e -> e.preventDefault())
-    prop.onKeyDown (fun e -> if e.key = "Enter" || e.key = " " then e.preventDefault())
-]
-
 let syncButton dispatch (wt: WorktreeStatus) (branchEvents: CardEvent list) (isPending: bool) (scopedKey: string) =
     if isPending then
         Html.button [
@@ -459,7 +454,7 @@ let syncButton dispatch (wt: WorktreeStatus) (branchEvents: CardEvent list) (isP
         ]
     else
         let syncing = isBranchSyncing branchEvents
-        let claudeBlocked = wt.CodingTool = Working || wt.CodingTool = WaitingForUser
+        let claudeBlocked = isCodingToolBusy wt
         let disabled = syncing || claudeBlocked
         if syncing then
             Html.button [
@@ -760,54 +755,6 @@ let newTabButton dispatch (wt: WorktreeStatus) =
         prop.text "+"
     ]
 
-let isCodingToolBusy (wt: WorktreeStatus) =
-    wt.CodingTool = Working || wt.CodingTool = WaitingForUser
-
-let commentIcon =
-    Svg.svg [
-        svg.className "btn-icon"
-        svg.viewBox (0, 0, 32, 32)
-        svg.fill "currentColor"
-        svg.children [
-            Svg.path [
-                svg.d "M231,273 C229.896,273 229,272.104 229,271 C229,269.896 229.896,269 231,269 C232.104,269 233,269.896 233,271 C233,272.104 232.104,273 231,273 L231,273 Z M223,273 C221.896,273 221,272.104 221,271 C221,269.896 221.896,269 223,269 C224.104,269 225,269.896 225,271 C225,272.104 224.104,273 223,273 L223,273 Z M215,273 C213.896,273 213,272.104 213,271 C213,269.896 213.896,269 215,269 C216.104,269 217,269.896 217,271 C217,272.104 216.104,273 215,273 L215,273 Z M223,257 C214.164,257 207,263.269 207,271 C207,275.419 209.345,279.354 213,281.919 L213,289 L220.009,284.747 C220.979,284.907 221.977,285 223,285 C231.836,285 239,278.732 239,271 C239,263.269 231.836,257 223,257 L223,257 Z"
-                svg.custom ("transform", "translate(-207, -257)")
-            ]
-        ]
-    ]
-
-let wrenchIcon =
-    Svg.svg [
-        svg.className "btn-icon"
-        svg.viewBox (0, 0, 64, 64)
-        svg.fill "currentColor"
-        svg.children [
-            Svg.path [ svg.d "M44.575,55.581l-16.411,-16.436c0,0 -8.462,2.97 -15.352,-3.931c-7.896,-7.908 -3.505,-17.121 -3.505,-17.121c0,0 8.199,8.117 10.347,10.269c1.71,1.713 3.686,1.47 7.043,-1.892c2.54,-2.544 3.233,-5.108 1.59,-6.754c-2.288,-2.291 -10.347,-10.269 -10.347,-10.269c0,0 9.763,-4.66 17.508,3.097c6.884,6.894 4.282,15.017 4.122,15.178c-0.064,0.063 7.343,7.509 16.341,16.505l-11.336,11.354Z" ]
-        ]
-    ]
-
-let createPrIcon =
-    Svg.svg [
-        svg.className "btn-icon"
-        svg.viewBox (0, 0, 15, 15)
-        svg.fill "currentColor"
-        svg.children [
-            Svg.path [ svg.d "M2.5 0C1.11929 0 0 1.11929 0 2.5C0 3.70948 0.85888 4.71836 2 4.94999V10.05C0.85888 10.2816 0 11.2905 0 12.5C0 13.8807 1.11929 15 2.5 15C3.88071 15 5 13.8807 5 12.5C5 11.2905 4.14112 10.2816 3 10.05V4.94999C4.14112 4.71836 5 3.70948 5 2.5C5 1.11929 3.88071 0 2.5 0Z" ]
-            Svg.path [ svg.d "M8.85355 0.853554L8.14645 0.146446L5.79289 2.5L8.14645 4.85355L8.85355 4.14645L7.70711 3H9.5C10.8807 3 12 4.11929 12 5.5V7.05001C10.8589 7.28164 10 8.29052 10 9.5C10 10.8807 11.1193 12 12.5 12C13.8807 12 15 10.8807 15 9.5C15 8.29052 14.1411 7.28164 13 7.05001V5.5C13 3.567 11.433 2 9.5 2H7.70711L8.85355 0.853554Z" ]
-        ]
-    ]
-
-let actionButton dispatch (wt: WorktreeStatus) (prompt: string) (title: string) (icon: ReactElement) =
-    let disabled = isCodingToolBusy wt
-    Html.button [
-        prop.className (if disabled then "action-btn disabled" else "action-btn")
-        prop.disabled disabled
-        yield! noFocusProps
-        prop.title (if disabled then $"{providerDisplayName wt.CodingToolProvider} is active" else title)
-        prop.onClick (fun e -> e.stopPropagation(); if not disabled then dispatch (LaunchAction (wt.Path, prompt)))
-        prop.children [ icon ]
-    ]
-
 let binIcon =
     Svg.svg [
         svg.className "btn-icon"
@@ -841,6 +788,11 @@ let deleteButton dispatch (wt: WorktreeStatus) =
 
 let archiveButton dispatch = ArchiveViews.archiveButton (ArchiveMsg >> dispatch)
 
+let prActionButton dispatch (wt: WorktreeStatus) (prompt: string) (title: string) (icon: ReactElement) =
+    let disabled = isCodingToolBusy wt
+    let disabledTitle = $"{providerDisplayName wt.CodingToolProvider} is active"
+    actionButton (fun () -> dispatch (LaunchAction (wt.Path, prompt))) disabledTitle disabled title icon
+
 let prBadgeContent dispatch (wt: WorktreeStatus) (repoName: string) (pr: PrInfo) =
     React.fragment [
         if pr.IsMerged then
@@ -866,21 +818,21 @@ let prBadgeContent dispatch (wt: WorktreeStatus) (repoName: string) (pr: PrInfo)
                     prop.text ($"{unresolved}/{total} threads")
                 ]
                 if unresolved > 0 then
-                    actionButton dispatch wt $"/pr {pr.Url}" "Fix PR comments" commentIcon
+                    prActionButton dispatch wt $"/pr {pr.Url}" "Fix PR comments" commentIcon
             | CountOnly total ->
                 Html.span [
                     prop.className (if total = 0 then "thread-badge dimmed" else "thread-badge")
                     prop.text ($"{total} comments")
                 ]
                 if total > 0 then
-                    actionButton dispatch wt $"/pr {pr.Url}" "Fix PR comments" commentIcon
+                    prActionButton dispatch wt $"/pr {pr.Url}" "Fix PR comments" commentIcon
             | _ -> ()
             pr.Builds |> List.map (fun build ->
                 React.fragment [
                     buildBadge repoName build
                     if build.Status = Failed then
                         match build.Url with
-                        | Some url -> actionButton dispatch wt $"/fix-build {url}" "Fix build" wrenchIcon
+                        | Some url -> prActionButton dispatch wt $"/fix-build {url}" "Fix build" wrenchIcon
                         | None -> ()
                 ]) |> React.fragment
     ]
@@ -897,7 +849,7 @@ let prRow dispatch (wt: WorktreeStatus) (repoName: string) =
         Html.div [
             prop.className "pr-row"
             prop.children [
-                actionButton dispatch wt "Commit all changes, push to origin, and create a pull request for this branch" "Create PR" createPrIcon
+                prActionButton dispatch wt "Commit all changes, push to origin, and create a pull request for this branch" "Create PR" createPrIcon
             ]
         ]
     | HasPr pr, _ ->
