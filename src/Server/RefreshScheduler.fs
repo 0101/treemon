2cb8adf33dfcacfd4e65880099c5441bc2506960
Petr Pokorny <petr@innit.cz>
2026-06-04 17:48:42 +0200

    [tm-canvas48-1hd] Extract CanvasScanner module from RefreshScheduler.fs
    Extracted inline CanvasScanner module to src/Server/CanvasScanner.fs as Server.CanvasScanner. Removed from RefreshScheduler.fs. Added to Server.fsproj before RefreshScheduler.fs.

--- src/Server/RefreshScheduler.fs
+++ src/Server/RefreshScheduler.fs
@@ -3,54 +3,9 @@ module Server.RefreshScheduler
 open System
 open System.Diagnostics
 open System.IO
-open System.Security.Cryptography
 open System.Threading
 open Shared
 open Shared.EventUtils
-
-module CanvasScanner =
-    let private canvasDir path = Path.Combine(path, ".agents", "canvas")
-
-    let private hashFile (filePath: string) =
-        use stream = File.OpenRead(filePath)
-        use sha = SHA256.Create()
-        sha.ComputeHash(stream)
-        |> Array.map _.ToString("x2")
-        |> String.Concat
-
-    let scan (worktreePath: string) : CanvasDoc list =
-        let dir = canvasDir worktreePath
-        if Directory.Exists(dir) then
-            let owners = CanvasDocOwnership.getAll worktreePath
-            Directory.GetFiles(dir, "*.html")
-            |> Array.sort
-            |> Array.map (fun filePath ->
-                let filename = Path.GetFileName(filePath)
-                { Filename = filename
-                  ContentHash = hashFile filePath
-                  LastModified = DateTimeOffset(File.GetLastWriteTimeUtc(filePath), TimeSpan.Zero)
-                  OwnerSessionId = owners |> Map.tryFind filename })
-            |> Array.toList
-        else
-            []
-
-    let tryCreateWatcher (post: CanvasDoc list -> unit) (worktreePath: string) : FileSystemWatcher option =
-        let dir = canvasDir worktreePath
-        let branch = Path.GetFileName(worktreePath)
-        if Directory.Exists(dir) then
-            let watcher = new FileSystemWatcher(dir, "*.html")
-            let handleEvent (eventType: string) (e: FileSystemEventArgs) =
-                Log.log "CanvasWatcher" $"{eventType}: {e.Name} in {branch}"
-                try scan worktreePath |> post
-                with _ -> ()
-            watcher.Changed.Add(handleEvent "Changed")
-            watcher.Created.Add(handleEvent "Created")
-            watcher.Deleted.Add(handleEvent "Deleted")
-            watcher.Renamed.Add(handleEvent "Renamed")
-            watcher.EnableRaisingEvents <- true
-            Some watcher
-        else
-            None
 
 type PerRepoState =
     { WorktreeList: GitWorktree.WorktreeInfo list
